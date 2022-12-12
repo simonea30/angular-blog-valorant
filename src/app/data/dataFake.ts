3a46f14ca66963d1643579b1a37f51a673de662e
simonea30 <simonealmeida2030@gmail.com>
2022-12-11 22:03:45 -0300

    first commit

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,43 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "VALORANT",
+    "description": "Os melhores jogadores",
+    "photoCover":"https://www.fnvsports.com.br/wp-content/uploads/2021/07/r704230_3_1296x729_16-9.png"
+  },
+  {
+    "id":"2",
+    "title": "Fade +",
+    "description": "INICIADORA",
+    "photoCover":"https://theclutch.com.br/wp-content/uploads/2022/04/fade-valorant-1.jpg"
+  },
+
+  {
+    "id":"3",
+    "title": "Yoru +",
+    "description": "DUELISTA",
+    "photoCover":"https://cdn.realsport101.com/images/ncavvykf/epicstream/abc7bff667886de65d139389f9585beebcec6bb8-1920x1200.jpg?auto=format"
+  },
+
+  {
+    "id":"4",
+    "title": "Raze +",
+    "description": "DUELISTA",
+    "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxBbQwniiM0RQCUyuRZryztdrQGSs3vHmxbg&usqp=CAU"
+  },
+
+  {
+    "id":"5",
+    "title": "Reyna +",
+    "description": "DUELISTA",
+    "photoCover":"https://www.pdvg.it/wp-content/uploads/2020/05/reyna-valorant_dd18.jpg"
+  },
+
+  {
+    "id":"6",
+    "title": "Killjoy +",
+    "description": "SENTINELA",
+    "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnUqwYOCRiBhsjdKW6f8jfBBKCYQ8Sog_aRh5X8P8_DoGWKMnoOS9G5vk3v_o_Gm99_Lo&usqp=CAU"
+  }
+
+]
